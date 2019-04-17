0d3974d0b127eb049649e3629706b743156f576e
Siddhant Goenka <sgoenka1@umbc.edu>
2019-04-17 14:48:06 -0400

    Multi Request mapping working. Used map. Tested.

--- server.go
+++ server.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"fmt"
-	"net"
 	"net/http"
 	"strconv"
 	"time"
 )
 
+/**
+ *	Reference:
+ *		https://gobyexample.com/worker-pools
+ */
+type HttpHandlerAdapter struct {
+	path string
+	handler http.HandlerFunc
+}
+
 type server struct {
 	ip string
 	port int16
 	sock *http.Server
+	rmap map[string]HttpHandlerAdapter
 }
-
-type HttpHandler func(http.ResponseWriter, *http.Request)
-type MsgListener net.Listener
 
 /**
  *	For the dynamic aspect, the http.Request contains all information
@@ -24,20 +30,49 @@ type MsgListener net.Listener
  *  with r.FormValue("email").
 */
 func (s *server) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(rw, "Welcome to my GoLang Server! ");
-	logger.Info(" GET / 200 OK ")
+
+	if r.URL.Path == "/" {
+		logger.Infof(" GET %s 200 OK ", r.URL.Path )
+		rw.WriteHeader(http.StatusOK);
+		fmt.Fprint(rw, "Welcome to my GoLang Server! ");
+	}else if val, ok := s.rmap[r.URL.Path]; ok {
+		rw.WriteHeader(http.StatusOK);
+		val.handler(rw, r)
+	} else {
+		rw.WriteHeader(http.StatusNotFound);
+		fmt.Fprintf(rw, " Error 404 Page Not Found ")
+		logger.Infof(" GET %s 404 NotFound ", r.URL.Path )
+	}
+
 }
 
-func myHttpHandler(h http.HandlerFunc) http.HandlerFunc {
-	return func(rw http.ResponseWriter, r *http.Request) {
-		logger.Infof(" GET %s 200 OK ", r.URL.Path )
-		h(rw, r)
-	}
+/**
+ *  Go multiplexes goroutines onto the available threads which is determined by the
+ *  GOMAXPROCS environment setting. As a result if this is set to 1 then a single
+ *  goroutine can hog the single thread. Go has available to it until it yields control back to the Go runtime.
+ *  More than likely doSomeBackgroundWork is hogging all the time on a single thread which is
+ *  preventing the http handler from getting scheduled.
+ *
+ *  First, as a general rule when using goroutines, you should set GOMAXPROCS to the number of
+ *  CPUs your system has or to whichever is bigger.
+ *
+ */
+func myHttpHandler(h http.HandlerFunc) HttpHandlerAdapter {
+
+	adapter := HttpHandlerAdapter {
+		handler: func (rw http.ResponseWriter, r *http.Request){
+			logger.Infof(" GET %s 200 OK ", r.URL.Path )
+			h(rw, r)
+		}};
+
+	return adapter;
 }
 
 func (s *server) register(path string, handler http.HandlerFunc ) {
 	logger.Infof(" Registering [%s] ", path)
-	http.HandleFunc(path, myHttpHandler(handler));
+	h := myHttpHandler(handler);
+	h.path = path
+	s.rmap[path] = h
 }
 
 
@@ -56,7 +91,7 @@ func (s *server) startWith(readTimeout int64, writeTimeout int64) error	{
 		MaxHeaderBytes: 1 << 20,
 	}
 
-	logger.Infof(" Starting server at %s@%d ", s.ip, s.port )
+	logger.Infof(" Go WebServer started at Port %d ", s.port )
 	return s.sock.ListenAndServe();
 }
 
@@ -72,9 +107,10 @@ func (s *server) stop() error	{
 func createServer(ip string, port int16) *server {
 
 	s := &server{ip:ip, port:port}
+	s.rmap = make(map[string]HttpHandlerAdapter)
 
 	//Register the handler
-	http.HandleFunc("/", s.ServeHTTP);
+	s.register("/", myHttpHandler(s.ServeHTTP).handler);
 	return s;
 
 }
